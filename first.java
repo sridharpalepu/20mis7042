2a151090f35d1ed6314be39d639f3604c9269b94
LAPTOP-KF8H60P9\sridh <sridhar.20mis7042@vitap.ac.in>
2021-03-10 19:02:47 +0530

    made changes to first.java program

---
+++ first.java
@@ -0,0 +1,15 @@
+
+sridhar sridhar public static void main(String args[]){
+	System.out.println("Hello");
+}
+class main{
+	public static void main(String args[]){
+		int a=10;
+		int b=20;
+		int sum=a+b;
+		System.out.println(sum);
+	}
+}
+public static void main(String args[]){
+	System.out.println(50/3);
+public add is
